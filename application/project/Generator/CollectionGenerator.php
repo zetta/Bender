93e49eadd40b3297429ec1e3313685c1e235be46
chentepixtol <chentepixtol@github.com>
2009-12-08 22:32:30 +0000

    Collections con las nuevos metodos

--- application/project/Generator/CollectionGenerator.php
+++ application/project/Generator/CollectionGenerator.php
@@ -21,15 +21,16 @@ class CollectionGenerator extends ModelGenerator
     {
         CommandLineInterface::getInstance()->printSection('Generator', 'Creating ' . $this->object.'Collection', 'NOTE');
         $this->template->set_filenames(array('collection' => 'Model/Collection'));
-        $this->template->assign('className', $this->object);
-        $this->template->assign('classVar', $this->getLowerObject());
-        #$this->template->assign('collection', $this->object . 'Collection');
         
         if($this->table->hasPrimaryField())
         {
         	$this->template->showBlock('hasPrimaryField');
         	$this->template->assign('primaryKeyGetter', $this->table->getPrimaryField()->getGetterName());
         	$this->template->assign('primaryKeyPhpName', $this->table->getPrimaryField()->getPhpName());
+        }
+        else 
+        {
+        	$this->template->showBlock('noPrimaryField');
         }
         
         $this->fileContent = $this->template->fetch('collection');
