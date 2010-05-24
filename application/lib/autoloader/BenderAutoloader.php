2530a60a7cebc9cf6fc2f272735c3f3b97932a7a
zetta <zetaweb@gmail.com>
2010-05-24 18:59:53 -0500

    problema con el autoloader

--- application/lib/autoloader/BenderAutoloader.php
+++ application/lib/autoloader/BenderAutoloader.php
@@ -7,7 +7,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
+require_once 'application/lib/autoloader/AutoloaderException.php';
+
 /**
  * BenderAutoloader class.
  *
